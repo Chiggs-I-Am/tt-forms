d27d9752dc6962823aa046946fd18a4abb59e8f9
Stephan Wilson <stephanthedev@gmail.com>
2022-07-01 22:24:19 +0000

    added story for dynamic-form

---
+++ stories/components/DynamicForm.stories.tsx
@@ -0,0 +1,21 @@
+import DynamicForm from "@components/dynamic-form";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import schema from "../forms/schema.json";
+import uischema from "../forms/uischema.json";
+
+export default
+{
+  title: "Components/Dynamic-Form",
+  component: DynamicForm,
+  /* parameters: {
+    layout: "fullscreen",
+  } */
+} as ComponentMeta<typeof DynamicForm>
+
+const Template: ComponentStory<typeof DynamicForm> = ( args ) => <DynamicForm { ...args } />;
+
+export const Form = Template.bind({});
+Form.args = {
+  schema: schema,
+  uischema: uischema
+};  
