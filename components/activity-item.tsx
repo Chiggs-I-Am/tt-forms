adf7148d801c74ee0e8cbb9d2dc4cfbf9b8af4ec
Stephan Wilson <stephanthedev@gmail.com>
2022-08-01 01:00:06 +0000

    updated styles

--- components/activity-item.tsx
+++ components/activity-item.tsx
@@ -12,14 +12,14 @@ interface ActivityItemProps
 export default function ActivityItem({ number, name, activities, slug, fee }: ActivityItemProps ) 
 {
   return (
-    <div className="max-w-md rounded-xl shadow-md overflow-hidden bg-primary-container-light">
-      <figure className="flex w-full h-full p-4 justify-between items-center gap-2">
+    <div className="rounded-xl shadow-md overflow-hidden bg-primary-container-light sm:max-w-sm">
+      <figure className="flex w-full min-h-[86px] p-4 justify-between items-center gap-2 select-none cursor-pointer">
         <figcaption className="flex flex-col items-start">
           <span className="text-xs text-outline-light font-medium">Form &#35;{ number }</span>
-          <p className="text-sm text-on-primary-container-light font-medium md:text-base">{ name }</p>
+          <p className="text-sm text-on-primary-container-light font-medium">{ name }</p>
           {/* <span className="text-xs text-on-primary-container-light font-medium">Fee: ${ fee }</span> */}
         </figcaption>
-        <div className="self-center">
+        <div className="hidden self-center sm:block">
           <Link href={`${ slug }`}>
             <a className="block w-fit h-fit rounded-full bg-primary-light select-none group hover:shadow-lg active:shadow-none">
               <span 
