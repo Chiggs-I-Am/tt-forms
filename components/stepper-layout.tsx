54941249db247cc3478eee4a85d95dd5557c8b91
Stephan Wilson <stephanthedev@gmail.com>
2022-08-01 20:41:16 +0000

    added container component and updated styles

--- components/stepper-layout.tsx
+++ components/stepper-layout.tsx
@@ -1,3 +1,4 @@
+import Container from "@components/container";
 import { Tab } from "@headlessui/react";
 import { and, Categorization, categorizationHasCategory, getAjv, isVisible, LayoutProps, optionIs, RankedTester, rankWith, uiTypeIs } from "@jsonforms/core";
 import { JsonFormsDispatch, useJsonForms, withJsonFormsLayoutProps } from "@jsonforms/react";
@@ -22,22 +23,23 @@ function StepperLayout( props: StepperLayoutProps )
   const ajv = getAjv({ jsonforms: { ...jsonFormState } });
 
   return (
-    <div className="grid gap-6 w-full max-w-md">
-      <Tab.Group selectedIndex={ selectedIndex } onChange={ setSelectedIndex }>
-        <Tab.List>
-        { categorization.elements.map( ( category ) => (
-            <Tab 
-              key={ category.label } 
-              className={ ({ selected }) => 
-                joinClassNames( !selected ? "hidden" : "" ,"w-full text-sm text-left text-white px-4 py-2 bg-primary-light" )}>
-              <span className="text-on-primary-light font-medium">{ category.label }</span>
-            </Tab>
-        ))}
-        </Tab.List>
+    <Container>
+      <div className="grid gap-5 grid-rows-[auto_1fr_auto] w-full max-w-md h-screen mx-auto">
+        <Tab.Group selectedIndex={ selectedIndex } onChange={ setSelectedIndex }>
+          <Tab.List>
+          { categorization.elements.map( ( category ) => (
+              <Tab
+                key={ category.label } 
+                className={ ({ selected }) => 
+                  joinClassNames( !selected ? "hidden" : "" , "w-full h-10 text-sm text-left text-white px-4 py-2 bg-primary-light" )}>
+                <span className="text-on-primary-light font-medium">{ category.label }</span>
+              </Tab>
+          ))}
+          </Tab.List>
           <Tab.Panels>
             { categorization.elements.map( ( category ) => 
               isVisible( category, data, path, ajv ) && (
-                <Tab.Panel as="ul" className="flex flex-col gap-6 min-h-[320px] px-2" key={ category.label }>                
+                <Tab.Panel as="ul" className="flex flex-col gap-4 px-4" key={ category.label }>
                   { category.elements.map( ( child, index ) => (
                     <li key={`${ path }-${ index }`}>
                       <JsonFormsDispatch
@@ -50,24 +52,25 @@ function StepperLayout( props: StepperLayoutProps )
               )
             )}
           </Tab.Panels>
-      </Tab.Group>
-      <div className="flex justify-end space-x-2">
-        <button 
-          disabled={ selectedIndex === 0 }
-          onClick={ () => {
-          if( selectedIndex > 0 ) setSelectedIndex( selectedIndex - 1 )
-        }}>Previous</button>
+        </Tab.Group>
+        <div className="flex py-4 justify-end space-x-2">
+          <button 
+            disabled={ selectedIndex === 0 }
+            onClick={ () => {
+            if( selectedIndex > 0 ) setSelectedIndex( selectedIndex - 1 )
+          }}>Previous</button>
 
-        <button 
-          className="flex w-fit h-fit rounded-full overflow-hidden shadow-lg text-sm text-primary-light font-medium"
-          disabled={ selectedIndex === categorization.elements.length - 1 }
-          onClick={ () => {
-            if( selectedIndex < categorization.elements.length - 1 ) setSelectedIndex( selectedIndex + 1 )
-          }}>
-          <span className="flex h-10 px-6 items-center justify-center">Next</span>
-        </button>
+          <button 
+            className="flex w-fit h-fit rounded-full overflow-hidden shadow-lg text-sm text-primary-light font-medium"
+            disabled={ selectedIndex === categorization.elements.length - 1 }
+            onClick={ () => {
+              if( selectedIndex < categorization.elements.length - 1 ) setSelectedIndex( selectedIndex + 1 )
+            }}>
+            <span className="flex h-10 px-6 items-center justify-center">Next</span>
+          </button>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
