556e77fdd07a9619796ba6e2a097405c04687d09
Stephan Wilson <stephanthedev@gmail.com>
2022-08-17 01:33:57 +0000

    added auth state provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '@styles/globals.css'
-import type { AppProps } from 'next/app'
+import { UserAuthStateProvider } from "@components/user-auth-state";
+import '@styles/globals.css';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+
+  return (
+    <UserAuthStateProvider>
+      <Component {...pageProps} />
+    </UserAuthStateProvider>
+  );
 }
 
 export default MyApp
