8a2e163a17bd04402e8311d89a2d65c97a0f8575
Stephan Wilson <stephanthedev@gmail.com>
2022-08-19 20:33:36 +0000

    updated Template

--- stories/pages/home.stories.tsx
+++ stories/pages/home.stories.tsx
@@ -1,43 +1,20 @@
 import Home from "@pages/index";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { SessionProvider } from "next-auth/react";
 
-export default
-{
+export default {
   title: "Pages/Home",
-  component: Home.contextTypes,
+  component: Home,
   parameters: {
     layout: "fullscreen",
   }
 } as ComponentMeta<typeof Home>;
 
-const Template: ComponentStory<typeof Home> = ( args: any ) => <Home { ...args }/>;
+const Template: ComponentStory<typeof Home> = ( args: any ) => (
+  <SessionProvider session={ args.session }>
+    <Home { ...args }/>
+  </SessionProvider>
+);
 
 export const HomePage = Template.bind({});
-HomePage.args = {
-  activities: [
-    { 
-      forms: [
-        { name: "Name search reservation", fee: 25, slug: "name-search-reservation" },
-        { name: "Notice of Directors", fee: 15, slug: "notice-of-directors" },
-      ], 
-      name: "Company registry",
-      imageURL: "https://picsum.photos/200",
-    },
-    {
-      forms: [
-        { name: "Name search reservation", fee: 25, slug: "name-search-reservation" },
-        { name: "Notice of Directors", fee: 15, slug: "notice-of-directors" },
-        { name: "Name search reservation", fee: 25, slug: "name-search-reservation" },
-      ],
-      name: "Land registry",
-      imageURL: "https://picsum.photos/200",
-    },
-    {
-      forms: [
-        { name: "Name search reservation", fee: 25, slug: "name-search-reservation" },
-      ],
-      name: "Civil registry",
-      imageURL: "https://picsum.photos/200",
-    },
-  ]
-};
+HomePage.args = {};
