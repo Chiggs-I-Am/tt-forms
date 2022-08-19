95e800744e9d663064b6967886bf1a69e3c8a89a
Stephan Wilson <stephanthedev@gmail.com>
2022-08-19 20:32:06 +0000

    updated imports and page styles

--- pages/auth/create-username.tsx
+++ pages/auth/create-username.tsx
@@ -1,8 +1,40 @@
+import CreateUsernameForm from "@components/auth/create-username-form";
+import Container from "@components/layout/container";
+import Layout from "@components/layout/layout";
+import { JsonSchema7, UISchemaElement } from "@jsonforms/core";
+import { GetServerSidePropsContext } from "next";
+import { useSession } from "next-auth/react";
+import { usernameFormSchema, usernameFormUiSchema } from "stories/forms/createUsernameFormSchema";
 
-export default function CreateUsername() {
+interface CreateUsernameProps
+{
+  schema: JsonSchema7;
+  uischema: UISchemaElement;
+}
+
+export default function CreateUsername({ schema, uischema }: CreateUsernameProps)
+{
+  const { data: session } = useSession();
+  
   return (
-    <>
-      {/* <CreateUsernameForm /> */}
-    </>
+    <Layout>
+      <Container>
+        <div className="grid gap-2 w-full h-full place-items-center">        
+          <CreateUsernameForm schema={ usernameFormSchema } uischema={ usernameFormUiSchema } user={ session?.userID }/>
+        </div>
+      </Container>
+    </Layout>
   )
+}
+
+export async function getServerSideProps({ req, res }: GetServerSidePropsContext )
+{
+  // const schemaDocRef = firestoreAdmin.collection( "forms" ).doc( "create_username" );
+  // const schemaDoc = await schemaDocRef.get();
+  // const schema = schemaDoc.data();
+  
+
+  return {
+    props: {}
+  };
 }
