77193db2a9abae524dafb452bd01123288e381cb
Stephan Wilson <stephanthedev@gmail.com>
2022-08-23 00:13:55 +0000

    updated data fetching

--- pages/auth/create-username.tsx
+++ pages/auth/create-username.tsx
@@ -2,9 +2,12 @@ import CreateUsernameForm from "@components/auth/create-username-form";
 import Container from "@components/layout/container";
 import Layout from "@components/layout/layout";
 import { JsonSchema7, UISchemaElement } from "@jsonforms/core";
+import { firestore } from "@libs/firebase/firestore";
+import { authOptions } from "@pages/api/auth/[...nextauth]";
+import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
+import { unstable_getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
-import { usernameFormSchema, usernameFormUiSchema } from "stories/forms/createUsernameFormSchema";
 
 interface CreateUsernameProps
 {
@@ -15,12 +18,13 @@ interface CreateUsernameProps
 export default function CreateUsername({ schema, uischema }: CreateUsernameProps)
 {
   const { data: session } = useSession();
+  const userID = session?.userID as string;
   
   return (
     <Layout>
       <Container>
         <div className="grid gap-2 w-full h-full place-items-center">        
-          <CreateUsernameForm schema={ usernameFormSchema } uischema={ usernameFormUiSchema } user={ session?.userID }/>
+          <CreateUsernameForm schema={ schema } uischema={ uischema } userID={ userID }/>
         </div>
       </Container>
     </Layout>
@@ -29,12 +33,45 @@ export default function CreateUsername({ schema, uischema }: CreateUsernameProps
 
 export async function getServerSideProps({ req, res }: GetServerSidePropsContext )
 {
-  // const schemaDocRef = firestoreAdmin.collection( "forms" ).doc( "create_username" );
-  // const schemaDoc = await schemaDocRef.get();
-  // const schema = schemaDoc.data();
+  let userSession = await unstable_getServerSession( req, res, authOptions );
+
   
+  if( !userSession ) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+      props: {}
+    };
+  }
+
+  let userID = userSession?.userID as string;
+  
+  let usernamesCollectionRef = collection( firestore, "usernames" );
+  let queryUsernameDoc = query( usernamesCollectionRef, where( "userID", "==", userID ));
+  let usernameDocSnapshot = await getDocs( queryUsernameDoc );
+  let isEmpty = usernameDocSnapshot.empty;
+  
+  // if user is signed in and has a username then redirect to home page
+  if( userSession && !isEmpty ) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+      props: {}
+    };
+  }
+  
+  let formDocRef = doc( firestore, "forms", "create-username" );
+  let formDocSnapshot = await getDoc( formDocRef );
+  let formDoc = formDocSnapshot.data();
 
   return {
-    props: {}
+    props: {
+      schema: formDoc?.schema,
+      uischema: formDoc?.uischema
+    }
   };
 }
