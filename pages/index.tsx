7a79afecec752df2f0ac1da4d22ef9c0c3b38d9b
Stephan Wilson <stephanthedev@gmail.com>
2022-08-25 02:50:40 +0000

    added auth dialog

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,12 @@ import { useCallback, useState } from "react";
 
 import ActivitiesList from "@components/activities-list";
 import ActivityItem from "@components/activity-item";
+import AuthCard from "@components/auth/auth-card";
+import { Transition } from "@headlessui/react";
 import { firestore } from "@libs/firebase/firestore";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { Session, unstable_getServerSession } from "next-auth";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import { authOptions } from "./api/auth/[...nextauth]";
 
 interface HomePageProps
@@ -24,7 +26,7 @@ interface HomePageProps
     registryForms: {
       [key: string]: {
         name: string;
-        fee: string;
+        fee: number;
         slug: string;
       }
     };
@@ -37,8 +39,10 @@ export default function Home({ activities, userSession }: HomePageProps )
   const [ activityName, setActivityName ] = useState<string>("");
   const [ forms, setForms ] = useState( [] as { name: string; fee: number; slug: string }[] );
   const [ isOpen, setIsOpen ] = useState( false );
+  const [ showAuthDialog, setShowAuthDialog ] = useState( false );
   
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
+  const session = userSession;
   
   const toggleSidbar = useCallback( ( ) => {
     setIsOpen( prev => !prev );
@@ -58,17 +62,18 @@ export default function Home({ activities, userSession }: HomePageProps )
 
   return (
     <Layout>
-      <div className="grid gap-6 w-full h-full grid-rows-[auto_1fr_auto]">
+      <div className="grid gap-6 w-full h-full grid-rows-[auto_1fr_auto] sm:gap-12">
         <AppToolbar>
-        { !session && !userSession ?
+        {/* { !session && !userSession ? */}
+        { !userSession ?
           <div>
             <button 
-              onClick={ () => signIn() }
-              className="flex items-center justify-center text-sm font-medium h-10 px-6 rounded-full shadow-md bg-primary-light text-on-primary-light">Sign in</button>
+              onClick={ () => setShowAuthDialog( true ) }
+              className="flex items-center justify-center text-sm font-medium h-10 px-6 rounded-full shadow-md bg-primary-light text-on-primary-light">Get started</button>
           </div>
           :
           <div className="flex items-center gap-2">
-            <h1 className="text-sm text-on-primary-container-light font-medium">{ session?.user?.email ?? userSession.user?.email }</h1>
+            <h1 className="text-sm text-on-primary-container-light font-medium">{ userSession.user?.email }</h1>
             <button 
               onClick={ handleSignOut }
               className="text-sm font-medium h-10 px-6 rounded-full shadow-md bg-primary-light text-on-primary-light">Sign out</button>
@@ -106,6 +111,33 @@ export default function Home({ activities, userSession }: HomePageProps )
               <ActivityFormList forms={ forms } />
           </Sidebar> 
         : null }
+
+        { showAuthDialog ?
+            <>
+              <Transition appear={ true } show={ showAuthDialog }>
+                <Transition.Child
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0">
+                  <div className="fixed inset-0 bg-black opacity-25"/>
+                </Transition.Child>
+
+                <Transition.Child>
+                  <div className="fixed inset-0 grid place-items-center">
+                    <AuthCard
+                      handleProviderSignIn={ ( provider ) => signIn( provider ) }
+                      handleEmailSignIn={ ( email ) => signIn( "email", { email }) }
+                      showCloseIcon={ true }
+                      handleCloseDialog={ () => setShowAuthDialog( false ) }/>
+                  </div>
+                </Transition.Child>
+              </Transition>
+            </>
+          : null
+        }
     </Layout>
   )
 }
@@ -125,24 +157,26 @@ export async function getServerSideProps({ req, res }: { req: NextApiRequest, re
       }
     };
   }
-
-  let userID = userSession.userID as string;
   
-  let usernamesCollectionRef = collection( firestore, "usernames" );
-  let queryUsernameDoc = query( usernamesCollectionRef, where( "userID", "==", userID ));
-  let usernameDocSnapshot = await getDocs( queryUsernameDoc );
-  let isEmpty = usernameDocSnapshot.empty;
-  
-  // if user is signed in and doesn't have a username then redirect to create-username page
-  if( userSession && isEmpty ) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/auth/create-username",
-      },
-      props: {}
-    };
+  try { 
+    let userID = userSession.userID as string;
+    let usernamesCollectionRef = collection( firestore, "usernames" );
+    let queryUsernameDoc = query( usernamesCollectionRef, where( "userID", "==", userID ));
+    let usernameDocSnapshot = await getDocs( queryUsernameDoc );
+    let isEmpty = usernameDocSnapshot.empty;
+    
+    // if user is signed in and doesn't have a username then redirect to create-username page
+    if( userSession && isEmpty ) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/auth/create-username",
+        },
+        props: {}
+      };
+    }
   }
+  catch( error: any ) {}
 
   return {
     props: {
