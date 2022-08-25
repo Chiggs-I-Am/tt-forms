82ed01a63bd1ec3307615e8d09b2fe47b358116d
Stephan Wilson <stephanthedev@gmail.com>
2022-08-25 18:00:00 +0000

    removed args

--- stories/pages/signin.stories.tsx
+++ stories/pages/signin.stories.tsx
@@ -5,31 +5,11 @@ export default
 {
   title: "Pages/SignIn",
   component: SignIn,
-  args: {
-    providers: {
-      email: {
-        name: "Email",
-        id: "email"
-      },
-      google: {
-        name: "Google",
-        id: "google"
-      },
-      facebook: {
-        name: "Facebook",
-        id: "facebook"
-      },
-      twitter: {
-        name: "Twitter",
-        id: "twitter"
-      }
-    }
-  },
   parameters: {
     layout: "fullscreen",
   }
 } as ComponentMeta<typeof SignIn>;
 
-const Template: ComponentStory<typeof SignIn> = ( args ) => <SignIn { ...args } />;
+const Template: ComponentStory<typeof SignIn> = () => <SignIn />;
 export const SigninPage = Template.bind({});
 SigninPage.args = {};
