a979d04e6b0182e661ce536076a53554ed44bf97
Stephan Wilson <stephanthedev@gmail.com>
2022-08-25 02:56:18 +0000

    updated home page

--- stories/pages/home.stories.tsx
+++ stories/pages/home.stories.tsx
@@ -7,14 +7,62 @@ export default {
   component: Home,
   parameters: {
     layout: "fullscreen",
+  },
+  args: {
+    userSession: {
+      user: {
+        name: "Stephan Wilson",
+        email: "email@domain.com",
+        image: "path-to-image",
+      },
+      expires: Date.now().toString(),
+    }
   }
 } as ComponentMeta<typeof Home>;
 
 const Template: ComponentStory<typeof Home> = ( args: any ) => (
-  <SessionProvider session={ args.session }>
+  <SessionProvider session={ args.userSession }>
     <Home { ...args }/>
   </SessionProvider>
 );
 
+const activities = [
+  {
+    name: "Company registry",
+    imageURL: "",
+    registryForms: {
+      nameSearchReservation: {
+        name: "Name search reservation",
+        fee: 25,
+        slug: "name-search-reservation"
+      }
+    }
+  },
+  {
+    name: "Company registry",
+    imageURL: "",
+    registryForms: {
+      nameSearchReservation: {
+        name: "Name search reservation",
+        fee: 25,
+        slug: "name-search-reservation"
+      }
+    }
+  },
+  {
+    name: "Company registry",
+    imageURL: "",
+    registryForms: {
+      nameSearchReservation: {
+        name: "Name search reservation",
+        fee: 25,
+        slug: "name-search-reservation"
+      }
+    }
+  },
+];
+
 export const HomePage = Template.bind({});
-HomePage.args = {};
+HomePage.args = {
+  activities
+};
