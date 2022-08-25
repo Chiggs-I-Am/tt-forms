947b7edfa9ce9f1b15f9227b47aae8d89553b343
Stephan Wilson <stephanthedev@gmail.com>
2022-08-25 02:52:22 +0000

    added logic to handle auth flow

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -2,6 +2,10 @@ import AuthCard from "@components/auth/auth-card";
 import Container from "@components/layout/container";
 import Layout from "@components/layout/layout";
 import { JsonSchema7, UISchemaElement } from "@jsonforms/core";
+import { authOptions } from "@pages/api/auth/[...nextauth]";
+import { GetServerSidePropsContext } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { signIn } from "next-auth/react";
 
 interface SigninProps
 {
@@ -15,15 +19,29 @@ export default function Signin({ schema, uischema }: SigninProps )
     <Layout>
       <Container>
         <div className="grid w-full h-full place-items-center">
-          <AuthCard />
+          <AuthCard 
+            handleProviderSignIn={ ( provider ) => signIn( provider ) }
+            handleEmailSignIn={ ( email ) => signIn( "email", { email } ) }/>
         </div>
       </Container>
     </Layout>
   )
 }
 
-export async function getStaticProps()
+export async function getServerSideProps( context: GetServerSidePropsContext )
 {
+  let session = await unstable_getServerSession( context.req, context.res, authOptions );
+
+  if( session ) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+      props: {}
+    }
+  }
+
   return {
     props: { }
   }
