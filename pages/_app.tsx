98ac4d1ca3482f1676297475292dcd16eb482d73
Stephan Wilson <stephanthedev@gmail.com>
2022-08-27 20:01:46 +0000

    added Toaster component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@styles/globals.css';
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from 'next/app';
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps)
 {
   return (
     <SessionProvider session={ session }>
       <Component { ...pageProps } />
+      <Toaster />
     </SessionProvider>
   );
 }
