89235e55b76f7b720dfee2fa60b270f98fae6fc7
Stephan Wilson <stephanthedev@gmail.com>
2022-08-27 19:44:53 +0000

    updated images domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [ "https://picsum.photos", "picsum.photos" ],
+    domains: [ "https://picsum.photos", "picsum.photos", "lh3.googleusercontent.com" ],
   },
 }
 
