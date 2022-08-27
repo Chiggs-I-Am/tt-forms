a7ee0ff5b3eb9b5befc7b4303bc61ca105234b29
Stephan Wilson <stephanthedev@gmail.com>
2022-08-27 19:47:57 +0000

    removed auth card

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,10 @@ import { useCallback, useState } from "react";
 
 import ActivitiesList from "@components/activities-list";
 import ActivityItem from "@components/activity-item";
-import AuthCard from "@components/auth/auth-card";
-import { Transition } from "@headlessui/react";
 import { firestore } from "@libs/firebase/firestore";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { Session, unstable_getServerSession } from "next-auth";
-import { signIn, signOut } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import { authOptions } from "./api/auth/[...nextauth]";
 
 interface HomePageProps
@@ -39,17 +37,9 @@ export default function Home({ activities, userSession }: HomePageProps )
   const [ activityName, setActivityName ] = useState<string>("");
   const [ forms, setForms ] = useState( [] as { name: string; fee: number; slug: string }[] );
   const [ isOpen, setIsOpen ] = useState( false );
-  const [ showAuthDialog, setShowAuthDialog ] = useState( false );
-  
-  // const { data: session } = useSession();
-  const session = userSession;
   
   const toggleSidbar = useCallback( ( ) => {
     setIsOpen( prev => !prev );
-  }, []);  
-
-  const handleSignOut = useCallback( () => {
-    signOut();
   }, []);
 
   const getActivityName = useCallback( ( name: string ) => {
@@ -63,23 +53,7 @@ export default function Home({ activities, userSession }: HomePageProps )
   return (
     <Layout>
       <div className="grid gap-6 w-full h-full grid-rows-[auto_1fr_auto] sm:gap-12">
-        <AppToolbar>
-        {/* { !session && !userSession ? */}
-        { !userSession ?
-          <div>
-            <button 
-              onClick={ () => setShowAuthDialog( true ) }
-              className="flex items-center justify-center text-sm font-medium h-10 px-6 rounded-full shadow-md bg-primary-light text-on-primary-light">Get started</button>
-          </div>
-          :
-          <div className="flex items-center gap-2">
-            <h1 className="text-sm text-on-primary-container-light font-medium">{ userSession.user?.email }</h1>
-            <button 
-              onClick={ handleSignOut }
-              className="text-sm font-medium h-10 px-6 rounded-full shadow-md bg-primary-light text-on-primary-light">Sign out</button>
-          </div>
-        }
-        </AppToolbar>
+        <AppToolbar userSession={ userSession } handleSignOut={ () => signOut() }></AppToolbar>
         <Container>
           <ActivitiesList>
             { activities.map( ( activity, index: number ) => {
@@ -111,33 +85,6 @@ export default function Home({ activities, userSession }: HomePageProps )
               <ActivityFormList forms={ forms } />
           </Sidebar> 
         : null }
-
-        { showAuthDialog ?
-            <>
-              <Transition appear={ true } show={ showAuthDialog }>
-                <Transition.Child
-                  enter="ease-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in duration-200"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0">
-                  <div className="fixed inset-0 bg-black opacity-25"/>
-                </Transition.Child>
-
-                <Transition.Child>
-                  <div className="fixed inset-0 grid place-items-center">
-                    <AuthCard
-                      handleProviderSignIn={ ( provider ) => signIn( provider ) }
-                      handleEmailSignIn={ ( email ) => signIn( "email", { email }) }
-                      showCloseIcon={ true }
-                      handleCloseDialog={ () => setShowAuthDialog( false ) }/>
-                  </div>
-                </Transition.Child>
-              </Transition>
-            </>
-          : null
-        }
     </Layout>
   )
 }
