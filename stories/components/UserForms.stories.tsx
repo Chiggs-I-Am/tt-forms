5c7434cb496a16ec3dbb24c52702bc587d9f133a
Stephan Wilson <stephanthedev@gmail.com>
2022-08-27 19:52:05 +0000

    added user forms story

---
+++ stories/components/UserForms.stories.tsx
@@ -0,0 +1,39 @@
+import UserForms from "@components/user/user-forms";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+export default {
+  title: "Components/User Forms",
+  component: UserForms
+} as ComponentMeta<typeof UserForms>;
+
+const Template: ComponentStory<typeof UserForms> = ( args ) => <UserForms { ...args } />;
+
+export const UserForm = Template.bind({});
+UserForm.args = {
+  forms: [
+    {
+      createdAt: "Thurs 25th Aug",
+      updatedAt: "Thurs 25th Aug",
+      status: "Pending Approval",
+      formData: {
+        nameSearchReservation: {}
+      }
+    },
+    {
+      createdAt: "Thurs 25th Aug",
+      updatedAt: "Thurs 25th Aug",
+      status: "Pending Approval",
+      formData: {
+        nameSearchReservation: {}
+      }
+    },
+    {
+      createdAt: "Thurs 25th Aug",
+      updatedAt: "Thurs 25th Aug",
+      status: "Pending Approval",
+      formData: {
+        nameSearchReservation: {}
+      }
+    },
+  ]
+};
