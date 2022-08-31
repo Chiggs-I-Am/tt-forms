72ad6cd46e61749fb5fdedafef7653c304477251
Stephan Wilson <stephanthedev@gmail.com>
2022-08-31 21:36:08 +0000

    added app-layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,19 @@
-import ActivityFormList from "@components/activity-form-list";
-import AppToolbar from "@components/layout/app-toolbar";
-import Container from "@components/layout/container";
-import Layout from "@components/layout/layout";
-import Sidebar from "@components/layout/sidebar";
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { useCallback, useState } from "react";
 
 
 import ActivitiesList from "@components/activities-list";
+import ActivityFormList from "@components/activity-form-list";
 import ActivityItem from "@components/activity-item";
+import AppLayout from "@components/layout/app-layout";
+import Container from "@components/layout/container";
+import Sidebar from "@components/layout/sidebar";
 import { firestore } from "@libs/firebase/firestore";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { Session, unstable_getServerSession } from "next-auth";
-import { signOut } from "next-auth/react";
 import { authOptions } from "./api/auth/[...nextauth]";
+import { NextPageWithLayout } from "./_app";
 
 interface HomePageProps
 {
@@ -32,88 +31,94 @@ interface HomePageProps
   userSession: Session;
 }
 
-export default function Home({ activities, userSession }: HomePageProps )
-{ 
-  const [ activityName, setActivityName ] = useState<string>("");
-  const [ forms, setForms ] = useState( [] as { name: string; fee: number; slug: string }[] );
-  const [ isOpen, setIsOpen ] = useState( false );
-  
-  const toggleSidbar = useCallback( ( ) => {
-    setIsOpen( prev => !prev );
+const Home: NextPageWithLayout<HomePageProps> = ({ activities, userSession }: HomePageProps) =>
+{
+  const [activityName, setActivityName] = useState<string>("");
+  const [forms, setForms] = useState([] as { name: string; fee: number; slug: string }[]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidbar = useCallback(() =>
+  {
+    setIsOpen(prev => !prev);
   }, []);
 
-  const getActivityName = useCallback( ( name: string ) => {
-    setActivityName( name )
+  const getActivityName = useCallback((name: string) =>
+  {
+    setActivityName(name)
   }, [])
 
-  const getFormsFromActivity = useCallback( ( forms: any ) => {
-    setForms( forms );
+  const getFormsFromActivity = useCallback((forms: any) =>
+  {
+    setForms(forms);
   }, []);
 
   return (
-    <Layout>
-      <div className="grid gap-6 w-full h-full grid-rows-[auto_1fr_auto] sm:gap-12">
-        <AppToolbar userSession={ userSession } handleSignOut={ () => signOut() }></AppToolbar>
-        <Container>
-          <ActivitiesList>
-            { activities.map( ( activity, index: number ) => {
-                const { name, imageURL, registryForms } = activity;
-                const numberOfForms = Object.keys( registryForms ).length;
-                const forms = Object.values( registryForms );
+    <Container>
+      <div className="mt-6">
+        <ActivitiesList>
+          { activities.map((activity, index: number) =>
+          {
+            const { name, imageURL, registryForms } = activity;
+            const numberOfForms = Object.keys(registryForms).length;
+            const forms = Object.values(registryForms);
 
-                return (
-                  <ActivityItem 
-                    key={ index } 
-                    name={ name }
-                    numberOfForms={ numberOfForms } 
-                    imageURL={ imageURL }
-                    handleOnClick={ () => {
-                      toggleSidbar();
-                      getFormsFromActivity( forms );
-                      getActivityName( name );
-                    }}/> 
-                  )
-            })}
-          </ActivitiesList>
-        </Container>
+            return (
+              <ActivityItem
+                key={ index }
+                name={ name }
+                numberOfForms={ numberOfForms }
+                imageURL={ imageURL }
+                handleOnClick={ () =>
+                {
+                  toggleSidbar();
+                  getFormsFromActivity(forms);
+                  getActivityName(name);
+                } } />
+            )
+          }) }
+        </ActivitiesList>
       </div>
-      { isOpen 
-        ? <Sidebar
-            open={ isOpen }
-            name={ `${ activityName } forms` }
-            handleOnClick={ toggleSidbar }>
-              <ActivityFormList forms={ forms } />
-          </Sidebar> 
-        : null }
-    </Layout>
+      { isOpen ? 
+        <Sidebar
+          open={ isOpen }
+          name={ `${ activityName } forms` }
+          handleOnClick={ toggleSidbar }>
+          <ActivityFormList forms={ forms } />
+        </Sidebar>
+        : null 
+      }
+    </Container>
   )
 }
 
 export async function getServerSideProps({ req, res }: { req: NextApiRequest, res: NextApiResponse })
 {
-  let activitiesCollectionRef = collection( firestore, "activities" );
-  let activitiesSnapshot = await getDocs( activitiesCollectionRef );
-  let activities = activitiesSnapshot.docs.map( doc => doc.data() );
+  let activitiesCollectionRef = collection(firestore, "activities");
+  let activitiesSnapshot = await getDocs(activitiesCollectionRef);
+  let activities = activitiesSnapshot.docs.map(doc => doc.data());
 
-  let userSession = await unstable_getServerSession( req, res, authOptions );
+  let userSession = await unstable_getServerSession(req, res, authOptions);
 
-  if( !userSession ) {
+  if (!userSession)
+  {
     return {
       props: {
         activities
       }
     };
   }
-  
-  try { 
+
+  try
+  {
     let userID = userSession.userID as string;
-    let usernamesCollectionRef = collection( firestore, "usernames" );
-    let queryUsernameDoc = query( usernamesCollectionRef, where( "userID", "==", userID ));
-    let usernameDocSnapshot = await getDocs( queryUsernameDoc );
+    let usernamesCollectionRef = collection(firestore, "usernames");
+    let queryUsernameDoc = query(usernamesCollectionRef, where("userID", "==", userID));
+    let usernameDocSnapshot = await getDocs(queryUsernameDoc);
     let isEmpty = usernameDocSnapshot.empty;
-    
+
     // if user is signed in and doesn't have a username then redirect to create-username page
-    if( userSession && isEmpty ) {
+    if (userSession && isEmpty)
+    {
       return {
         redirect: {
           permanent: false,
@@ -123,7 +128,7 @@ export async function getServerSideProps({ req, res }: { req: NextApiRequest, re
       };
     }
   }
-  catch( error: any ) {}
+  catch (error: any) { }
 
   return {
     props: {
@@ -132,3 +137,7 @@ export async function getServerSideProps({ req, res }: { req: NextApiRequest, re
     }
   };
 }
+
+Home.Layout = AppLayout
+
+export default Home;
