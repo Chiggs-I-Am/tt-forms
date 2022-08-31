040def55e3ad1060741f6072b72d8f4c8216434b
Stephan Wilson <stephanthedev@gmail.com>
2022-08-31 22:16:06 +0000

    added dark mode styles

--- pages/auth/verify-request.tsx
+++ pages/auth/verify-request.tsx
@@ -5,13 +5,13 @@ export default function VerifyRequest() {
   return (
     <Layout>
       <div className="grid w-full h-full place-items-center">
-        <div className="w-full max-w-sm rounded-xl shadow-lg overflow-hidden bg-primary-container-light">
+        <div className="w-full max-w-sm rounded-xl shadow-lg overflow-hidden dark:bg-primary-dark bg-primary-container-light">
           <div className="grid w-full gap-4 p-4">
             <div>
-              <h1 className="text-2xl text-on-primary-container-light font-semibold">Check your email</h1> 
+              <h1 className="text-2xl dark:text-on-primary-dark text-on-primary-container-light font-semibold">Check your email</h1> 
             </div>
             <div>
-              <p className="text-sm text-on-primary-container-light">A sign in link has been sent to your email address.</p>
+              <p className="text-sm dark:text-on-primary-dark text-on-primary-container-light">A sign in link has been sent to your email address.</p>
               <Link href="/">
                 <a className="text-sm text-tertiary-light font-bold hover:underline">Go to home page.</a>
               </Link>
