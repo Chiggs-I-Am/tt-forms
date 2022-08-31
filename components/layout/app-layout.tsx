5cc459820d4b1f039f31c7b8f36f0de525edbc16
Stephan Wilson <stephanthedev@gmail.com>
2022-08-31 21:35:57 +0000

    added app-layout

---
+++ components/layout/app-layout.tsx
@@ -0,0 +1,31 @@
+import { ReactElement, useState } from 'react';
+import useSWR from "swr";
+import AppToolbar from "./app-toolbar";
+
+async function getUserSession( endpoint: string ) {
+  let response = await fetch( endpoint );
+  return response.json();
+}
+
+export default function AppLayout( page: ReactElement )
+{
+  const { data, error } = useSWR( "/api/auth/session", getUserSession );
+  const [ showAuthDialog, setShowAuthDialog ] = useState( false );
+
+  const AuthDialog = ({ children }: any ) => (
+    <>{ children }</>
+  );
+
+  return (
+    <div className="grid grid-flow-row auto-rows-max">
+      <AppToolbar userSession={ data } handleSignIn={ () => setShowAuthDialog( true ) } />
+      {/* NOTE: height = viewport height - navbar height */}
+      <main className="grid h-[calc(100vh-56px)]">
+        { page }
+      </main>
+      <footer className="grid place-items-center dark:bg-primary-container-dark">
+        <div className="flex items-center h-12 px-4 text-sm dark:text-on-primary-container-dark font-medium">Otaku Mode</div>
+      </footer>
+    </div >
+  )
+}
