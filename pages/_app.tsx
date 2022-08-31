988321b8ac25ddc4f8cbaec001991452f2a461b6
Stephan Wilson <stephanthedev@gmail.com>
2022-08-31 21:33:58 +0000

    updated _app to share single layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,49 @@
 import '@styles/globals.css';
+import { NextPage } from "next";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from "react";
 import { Toaster } from "react-hot-toast";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps)
+interface NoopProps
 {
-  return (
+  children: ReactElement
+}
+
+function Noop({ children }: NoopProps ) {
+  return <>{ children }</>;
+}
+
+export type NextPageWithLayout<Page = {}, IPage = Page> = NextPage<Page, IPage> & {
+  Layout?: ( page: ReactElement ) => ReactNode
+};
+
+interface AppWithLayoutProps extends AppProps
+{
+  Component: NextPageWithLayout;
+}
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppWithLayoutProps )
+{
+  const Layout = Component.Layout ?? ( page => page );
+
+  return Layout(
     <SessionProvider session={ session }>
       <Component { ...pageProps } />
-      <Toaster />
+      <Toaster toastOptions={ {
+        success: {
+          style: {
+            background: "#e3dfff",
+            color: "#100069"
+          },
+        },
+        error: {
+          style: {
+            background: "#ffdad6",
+            color: "410002",
+          }
+        }
+      } } />
     </SessionProvider>
   );
 }
