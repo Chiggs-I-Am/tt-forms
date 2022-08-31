d5bf48de333245a1b6a7574675562f926c108393
Stephan Wilson <stephanthedev@gmail.com>
2022-08-31 21:37:29 +0000

    updated args data

--- stories/pages/create-username.stories.tsx
+++ stories/pages/create-username.stories.tsx
@@ -1,6 +1,7 @@
 import CreateUsername from "@pages/auth/create-username";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { SessionProvider } from "next-auth/react";
+import { usernameFormSchema, usernameFormUiSchema } from "stories/forms/createUsernameFormSchema";
 
 export default {
   title: "Pages/Create Username",
@@ -10,11 +11,23 @@ export default {
   }
 } as ComponentMeta<typeof CreateUsername>;
 
+const session = {
+  user: {
+    name: "Stephan Wilson",
+    email: "stephanthedev@gmail.com",
+    image: "path-to-image",
+  },
+  expires: Date.now().toString()
+};
+
 const Template: ComponentStory<typeof CreateUsername> = ( args: any ) => (
-  <SessionProvider session={ args.session }>
+  <SessionProvider session={ session }>
     <CreateUsername { ...args }/>
   </SessionProvider>
 );
 
 export const CreateUsernamePage = Template.bind({});
-CreateUsernamePage.args = {};
+CreateUsernamePage.args = {
+  schema: usernameFormSchema,
+  uischema: usernameFormUiSchema
+};
