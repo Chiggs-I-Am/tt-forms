322a116e862eb13696edeb5041d0979dd1ab1400
Stephan Wilson <stephanthedev@gmail.com>
2022-09-02 00:17:12 +0000

    updated styles

--- components/activity-form-list.tsx
+++ components/activity-form-list.tsx
@@ -17,10 +17,10 @@ export default function ActivityFormList({ forms }: ActivityFormListProps )
         const { name, fee, slug } = form;
         return (
           <Link key={ index } href={ `form/${ slug }` }>
-            <a className="w-full h-fit rounded-xl shadow-md overflow-hidden dark:bg-primary-container-dark bg-primary-container-light">
+            <a className="w-full h-fit rounded-xl shadow-md overflow-hidden dark:bg-primary-dark bg-primary-container-light">
               <figure className="flex min-h-[86px] p-4 justify-between items-center gap-2 select-none cursor-pointer">
                 <figcaption className="flex flex-col items-start">
-                  <p className="text-sm dark:text-on-primary-container-dark text-on-primary-container-light font-bold">{ name }</p>
+                  <p className="text-sm dark:text-on-primary-dark text-on-primary-container-light font-bold">{ name }</p>
                   <p className="text-xs dark:text-outline-dark text-on-primary-container-light font-semibold">Fee: ${ fee }</p>
                 </figcaption>
               </figure>
