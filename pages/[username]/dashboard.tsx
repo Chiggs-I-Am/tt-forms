f89f066132421c766fb8de0c9b2cc6d452768e72
Stephan Wilson <stephanthedev@gmail.com>
2022-09-02 00:20:30 +0000

    removed next-auth added firebase-auth & app-layout

--- pages/[username]/dashboard.tsx
+++ pages/[username]/dashboard.tsx
@@ -1,18 +1,15 @@
-import AppToolbar from "@components/layout/app-toolbar";
+import { useAuthState } from "@components/auth/user-auth-state";
+import AppLayout from "@components/layout/app-layout";
 import Container from "@components/layout/container";
-import Layout from "@components/layout/layout";
 import UserForms from "@components/user/user-forms";
 import UserProfile from "@components/user/user-profile";
 import { firestore } from "@libs/firebase/firestore";
-import { authOptions } from "@pages/api/auth/[...nextauth]";
+import { NextPageWithLayout } from "@pages/_app";
 import { collection, doc, getDoc, getDocs, Timestamp } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
-import { Session, unstable_getServerSession } from "next-auth";
-import { signOut } from "next-auth/react";
 
 interface UsernamePageProps
 {
-  userSession: Session;
   forms: {
     name: string;
     status: string;
@@ -22,40 +19,33 @@ interface UsernamePageProps
   }[]
 }
 
-export default function UsernamePage({ userSession, forms }: UsernamePageProps )
+const UserDashboard: NextPageWithLayout<UsernamePageProps> = ( { forms }: UsernamePageProps ) =>
 {
-  const { user } = userSession;
+  const { user } = useAuthState();
 
   return (
-    <Layout>
-      <div className="grid gap-6 w-full h-full grid-rows-[auto_1fr_auto] sm:gap-12">
-        <AppToolbar userSession={ userSession } handleSignOut={ () => signOut() }></AppToolbar>
-        <Container>
-          <div className="flex flex-col w-full gap-6 items-center">
-            <UserProfile name={ user?.name! } email={ user?.email! } image={ user?.image! } />
-            <div className="w-full max-w-sm px-4">
-              <h3 className="text-xs font-medium dark:text-on-surface-variant-dark text-on-surface-light py-4">Completed Forms</h3>
-              <UserForms forms={ forms } />
-            </div>
-          </div>
-        </Container>
+    <Container>
+      <div className="flex flex-col w-full gap-6 items-center mt-6">
+        { user && <UserProfile name={ user?.displayName! } email={ user?.email! } image={ user?.photoURL! } /> }
+        <div className="w-full max-w-sm px-4">
+          <h3 className="text-xs font-medium dark:text-on-surface-variant-dark text-on-surface-light py-4">Completed Forms</h3>
+          { forms.length === 0 ?
+            <div className="text-sm dark:text-on-surface-dark text-on-surface-light">👀 Nothing to see here...</div>
+            :
+            <UserForms forms={ forms } />
+          }
+        </div>
       </div>
-    </Layout>
+    </Container>
   );
 }
 
-export async function getServerSideProps({ req, res, query }: GetServerSidePropsContext )
-{
-  let userSession = await unstable_getServerSession( req, res, authOptions );
-  if( !userSession ) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/",
-      }
-    };
-  }
+UserDashboard.Layout = AppLayout;
 
+export default UserDashboard;
+
+export async function getServerSideProps( { req, res, query }: GetServerSidePropsContext )
+{
   const { username } = query;
 
   const usernameDocRef = doc( firestore, "usernames", username as string );
@@ -65,14 +55,15 @@ export async function getServerSideProps({ req, res, query }: GetServerSideProps
   let userID = null;
   let forms = null
 
-  if( usernameDocSnapshot.exists() ) {
+  if ( usernameDocSnapshot.exists() ) {
     userID = usernameDoc?.userID;
 
     // get all forms the user completed
     const userFormsCollectionRef = collection( firestore, "users", userID, "forms" );
     const userFormsDocSnapshot = await getDocs( userFormsCollectionRef );
-    
-    forms = userFormsDocSnapshot.docs.map( doc => { 
+
+    forms = userFormsDocSnapshot.docs.map( doc =>
+    {
       let data = doc.data();
       let createdAt = getDateFromTimestamp( data.createdAt )
       let updatedAt = getDateFromTimestamp( data.updatedAt )
@@ -83,16 +74,15 @@ export async function getServerSideProps({ req, res, query }: GetServerSideProps
         createdAt,
         updatedAt,
       };
-      
+
       return form;
-    });
+    } );
   }
 
   console.log( "FORMS", forms );
 
   return {
     props: {
-      userSession,
       forms
     }
   };
