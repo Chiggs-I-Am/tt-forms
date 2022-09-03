dcff9983d6669b0e60d6c481cfd465cae06e14e0
Stephan Wilson <stephanthedev@gmail.com>
2022-09-03 19:01:24 +0000

    updated props

--- components/user/user-profile.tsx
+++ components/user/user-profile.tsx
@@ -1,28 +1,31 @@
+import { User } from "firebase/auth";
 import Image from "next/image";
 
 interface UserProfileProps
 {
-  name: string;
-  email: string;
-  image: string;
+  user: User & { username: string | undefined; };
 }
 
-export default function UserProfile({ name, email, image }: UserProfileProps ) {
+export default function UserProfile({ user }: UserProfileProps ) {
+  const { displayName, email, photoURL, username } = user;
+
   return (
-    <div className="w-full max-w-xs rounded-lg shadow-lg overflow-hidden dark:bg-primary-dark bg-primary-container-light">
+    <div className="w-full max-w-xs rounded-lg shadow-lg overflow-hidden dark:bg-secondary-container-dark bg-primary-container-light">
       <div className="grid gap-4 w-full p-4">
         <div className="flex justify-center">
           <div className="relative w-14 h-14 rounded-full overflow-hidden">
             <Image
               alt="Profile image"
-              src={ `${ image }` }
+              src={ `${ photoURL }` }
               objectFit="cover"
-              layout="fill"/>
+              layout="fill"
+              priority />
           </div>
         </div>
         <div className="text-center">
-          <h3 className="text-lg dark:text-on-primary-dark text-on-primary-container-light font-medium">{ `${ name }` }</h3>
-          <p className="text-xs">{ `${ email }` }</p>
+          <h3 className="text-lg dark:text-on-secondary-container-dark text-on-primary-container-light font-semibold">{ `${ displayName }` }</h3>
+          <p className="text-xs font-medium dark:text-outline-dark outline-outline-light">{ `@${ username ?? "Username" }` }</p>
+          <p className="text-xs dark:text-outline-dark outline-outline-light">{ `${ email }` }</p>
         </div>
       </div>
     </div>
