42f5fb799af7a8ffd6b0ba1ca0903ef34bf23ddf
Stephan Wilson <stephanthedev@gmail.com>
2022-09-10 20:13:13 +0000

    updated props

--- components/user/user-forms.tsx
+++ components/user/user-forms.tsx
@@ -1,4 +1,3 @@
-import { lowerCase, upperFirst } from "lodash";
 
 interface UserFormsProps
 {
@@ -12,11 +11,12 @@ interface UserFormsProps
 }
 
 export default function UserForms({ forms }: UserFormsProps ) {
+
   return (
     <div>
       { forms.map( ( form, index: number ) => {
-        const { formData } = form;
-
+        const { name, createdAt, status, formData } = form;
+        
           return (
             <div 
               key={ index }
@@ -24,14 +24,14 @@ export default function UserForms({ forms }: UserFormsProps ) {
               
               <div className="grid">
                 <div className="px-2">
-                  <h2 className="text-base dark:text-on-surface-variant-dark text-on-surface-light font-semibold">{ upperFirst( lowerCase(form.name) ) }</h2>
+                  <h2 className="text-base dark:text-on-surface-variant-dark text-on-surface-light font-semibold">{ name }</h2>
                 </div>
                 <div className="grid px-2 pb-2 dark:text-outline-dark text-outline-light">
                   <div className="text-xs">
-                    <span className="font-medium">Date applied: </span><span>{ form.createdAt }</span>
+                    <span className="font-medium">Date applied: </span><span>{ createdAt }</span>
                   </div>
                   <div className="text-xs">
-                    <span className="font-medium">Status: </span><span>{ form.status }</span>
+                    <span className="font-medium">Status: </span><span>{ status }</span>
                   </div>
                 </div>
               </div>
