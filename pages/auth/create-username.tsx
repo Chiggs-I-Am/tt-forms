4fa8502a2d301d7c11c46caa777db63d3cd04faa
Stephan Wilson <stephanthedev@gmail.com>
2022-09-11 02:45:30 +0000

    removed server code

--- pages/auth/create-username.tsx
+++ pages/auth/create-username.tsx
@@ -1,10 +1,9 @@
 import CreateUsernameForm from "@components/auth/create-username-form";
 import { useAuthState } from "@components/auth/user-auth-state";
+import { createUsernameSchema, createUsernameUischema } from "@components/form/schemas/create-username";
 import Container from "@components/layout/container";
 import Layout from "@components/layout/layout";
 import { JsonSchema7, UISchemaElement } from "@jsonforms/core";
-import { firestore } from "@libs/firebase/firestore";
-import { doc, getDoc } from "firebase/firestore";
 
 interface CreateUsernameProps
 {
@@ -13,7 +12,7 @@ interface CreateUsernameProps
   userID: string;
 }
 
-export default function CreateUsername({ schema, uischema }: CreateUsernameProps)
+export default function CreateUsername()
 {
   let { user } = useAuthState();
 
@@ -22,39 +21,9 @@ export default function CreateUsername({ schema, uischema }: CreateUsernameProps
       <Container>
         <div className="grid gap-2 w-full h-full place-items-center">
           {/* TODO: pull out firestore logic from form */}
-          <CreateUsernameForm schema={ schema } uischema={ uischema } userID={ `${ user?.uid }` }/>
+          <CreateUsernameForm schema={ createUsernameSchema } uischema={ createUsernameUischema } userID={ `${ user?.uid }` }/>
         </div>
       </Container>
     </Layout>
   )
-}
-
-export async function getStaticProps()
-{
-  /* let usernamesCollectionRef = collection( firestore, "usernames" );
-  let queryUsernameDoc = query( usernamesCollectionRef, where( "userID", "==", `${ currentUser?.uid }` ));
-  let usernameDocSnapshot = await getDocs( queryUsernameDoc );
-  let isEmpty = usernameDocSnapshot.empty; */
-  
-  // if user is signed in and has a username then redirect to home page
-  /* if( currentUser && !isEmpty ) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/",
-      },
-      props: {}
-    };
-  } */
-  
-  let formDocRef = doc( firestore, "forms", "create-username" );
-  let formDocSnapshot = await getDoc( formDocRef );
-  let formDoc = formDocSnapshot.data();
-
-  return {
-    props: {
-      schema: formDoc?.schema,
-      uischema: formDoc?.uischema
-    }
-  };
 }
