e804ad208e0a38ae86c0fbc490cb9f86a53ca8c2
Stephan Wilson <stephanthedev@gmail.com>
2022-09-16 01:33:25 +0000

    remove unused code

--- components/layout/app-layout.tsx
+++ components/layout/app-layout.tsx
@@ -1,25 +1,19 @@
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 import AppToolbar from "./app-toolbar";
-
-async function getUser( endpoint: string ) {
-  let response = await fetch( endpoint );
-  return response.json();
-}
 
 export default function AppLayout( page: ReactElement )
 {
-  const [ showAuthDialog, setShowAuthDialog ] = useState( false );
-  
-  // const username = 
-
   return (
     <div className="layout">
       <AppToolbar />
       <main className="main grid h-full">
         { page }
       </main>
-      <footer className="footer grid place-items-center dark:bg-primary-container-dark">
-        <div className="flex items-center h-14 px-4 text-sm dark:text-on-primary-container-dark font-medium">Otaku Mode</div>
+      <footer className="footer grid place-items-center dark:bg-primary-container-dark bg-primary-light">
+        <div className="flex flex-col justify-center items-center h-14 px-4 text-sm dark:text-on-primary-container-dark font-medium">
+          Otaku Mode
+          <span className="text-xs dark:text-on-primary-container-dark text-on-primary-light">stephanthedev@gmail.com</span>
+        </div>
       </footer>
     </div>
   )
