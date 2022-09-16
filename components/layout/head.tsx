136710e4e8c830377c06167b0685665a79c7e375
Stephan Wilson <stephanthedev@gmail.com>
2022-09-16 16:00:18 +0000

    added head component

---
+++ components/layout/head.tsx
@@ -0,0 +1,9 @@
+import SEO from "./seo"
+
+export default function Head() {
+  return (
+    <SEO>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </SEO>
+  )
+}
