0289a0c080be7263400ba3f4a5fa4f1f13c6579d
Stephan Wilson <stephanthedev@gmail.com>
2023-12-05 08:23:57 -0400

    updated joinClasses

--- utils/joinClasses.ts
+++ utils/joinClasses.ts
@@ -1,6 +1,19 @@
-export default function useJoinClassNames()
-{
-  let joinedClasses = (  ...classes: string[] ) => classes.filter(Boolean).join(" ");
+import { twMerge } from "tailwind-merge";
 
-  return joinedClasses;
+/* 
+  This function is used to merge classnames together.
+
+  @param args - The classnames to merge.
+
+  @example
+    import { joinClasses } from 'utils/joinClasses';
+    joinClasses('text-red-500', null, 'text-xl');
+    // => 'text-red-500 text-xl'
+
+  @returns The merged classnames.
+*/
+export function joinClasses(...args: unknown[]) {
+  const classes = args.filter(Boolean).join(' ');
+  // twMerge removes any duplicates
+  return twMerge( classes );
 }
