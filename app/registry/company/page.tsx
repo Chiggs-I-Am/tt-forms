9e853dcfb9e6a18654a69c1019c310d7ac29f019
Stephan Wilson <stephanthedev@gmail.com>
2023-12-05 08:35:16 -0400

    page: registry/company

---
+++ app/registry/company/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="[grid-area:main/fullbleed]">
+      <section className="[grid-area:main]">
+        Company registry
+      </section>
+    </main>
+  )
+}
