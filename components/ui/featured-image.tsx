824a8cc93b85264755b055958a5e74abedb35741
Stephan Wilson <stephanthedev@gmail.com>
2023-12-06 08:19:32 -0400

    updated styles

--- components/ui/featured-image.tsx
+++ components/ui/featured-image.tsx
@@ -5,11 +5,11 @@ export default function FeaturedImage() {
 		<figure className="relative grid place-items-center w-full h-80 rounded-lg overflow-hidden [&>*]:col-span-full [&>*]:row-span-full shadow-md">
 			<div className="w-full h-full dark:bg-[#111210]/60 z-10" />
 			<figcaption className="z-20 px-4">
-				<h1 className="max-w-[24ch] text-4xl">Filling out a form has never been easier.</h1>
-				<sub className="uppercase font-medium">Online forms made easy</sub>
+				<h1 className="max-w-[24ch] text-4xl dark:text-[#ede0de]">Filling out a form has never been easier.</h1>
+				<sub className="dark:text-[#ede0de] uppercase font-medium">Online forms made easy</sub>
 			</figcaption>
 			<Image
-				src="https://picsum.photos/1024/1024"
+				src="/images/government_plaza.jpg"
 				alt="Hero image"
 				className="-z-10 object-cover"
 				fill
--- components/ui/featured-item.tsx
+++ components/ui/featured-item.tsx
@@ -16,7 +16,7 @@ export default function FeaturedItem({ href, children, title, imageSrc }: Featur
 			<div className="relative grid w-full place-items-center text-xs uppercase aspect-video rounded-lg overflow-hidden border border-solid dark:border-[#a08c8b] hover:dark:border-[#ffb3ae] [&>*]:col-span-full [&>*]:row-span-full group">
 				<div className="grid place-items-center">
 					{ children }
-					<h3 className="text-sm dark:text-[#ede0de] font-medium">{title}</h3>
+					<h3 className="text-xs dark:text-[#ede0de] font-medium">{title}</h3>
 				</div>
 				<div className="w-full h-full dark:bg-[#201a1a]/60 -z-10 group-hover:dark:bg-[#201a1a]/40 transition-all" />
 				<Image
