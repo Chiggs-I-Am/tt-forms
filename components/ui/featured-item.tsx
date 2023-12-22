b87ee51cbf2d011718bed6d3427f3587dbc9b0af
Stephan Wilson <stephanthedev@gmail.com>
2023-12-22 16:49:28 -0400

    updated styles

--- components/ui/featured-item.tsx
+++ components/ui/featured-item.tsx
@@ -1,32 +1,47 @@
+import { joinClasses } from "@/utils/joinClasses";
+import { Noto_Sans } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 
-interface FeaturedItemProps
-{
+const notoSans = Noto_Sans({ subsets: ["latin"] });
+
+interface FeaturedItemProps {
   href: string;
   children: ReactNode;
   title: string;
   imageSrc: string;
 }
 
-export default function FeaturedItem({ href, children, title, imageSrc }: FeaturedItemProps) {
-	return (
-		<Link href={href}>
-			<div className="relative grid w-full place-items-center text-xs uppercase aspect-video rounded-lg overflow-hidden border border-solid dark:border-[#a08c8b] hover:dark:border-[#ffb3ae] [&>*]:col-span-full [&>*]:row-span-full group">
-				<div className="grid place-items-center">
-					{ children }
-					<h3 className="text-xs dark:text-[#ede0de] font-medium">{title}</h3>
-				</div>
-				<div className="w-full h-full dark:bg-[#201a1a]/60 -z-10 group-hover:dark:bg-[#201a1a]/40 transition-all" />
-				<Image
-					src={imageSrc}
-					alt="company registry"
-					className="-z-20 group-hover:scale-125 transition-all"
-					fill
+export default function FeaturedItem({
+  href,
+  children,
+  title,
+  imageSrc,
+}: FeaturedItemProps) {
+  return (
+    <Link href={href}>
+      <div className="group relative grid aspect-video h-full w-full place-items-center overflow-hidden rounded-lg text-xs uppercase shadow-2 *:col-span-full *:row-span-full">
+        <div className="grid place-items-center text-gray-12 dark:text-[#ede0de]">
+          {children}
+          <h3
+            className={joinClasses(
+              notoSans.className,
+              "text-center text-xs font-bold dark:font-medium",
+            )}
+          >
+            {title}
+          </h3>
+        </div>
+        <div className="-z-10 h-full w-full bg-[var(--gray-1)] opacity-55 group-hover:opacity-35 transition-colors dark:opacity-70 group-hover:dark:opacity-40" />
+        <Image
+          src={imageSrc}
+          alt="company registry"
+          className="oject-cover -z-20 transition-all group-hover:scale-125"
+          fill
           priority
-				/>
-			</div>
-		</Link>
-	);
+        />
+      </div>
+    </Link>
+  );
 }
