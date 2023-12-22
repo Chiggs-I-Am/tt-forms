c54261be26396dfc580027446412c3c48e3bab6d
Stephan Wilson <stephanthedev@gmail.com>
2023-12-22 16:45:52 -0400

    updated styles

--- components/ui/featured-image.tsx
+++ components/ui/featured-image.tsx
@@ -1,19 +1,25 @@
+import { joinClasses } from "@/utils/joinClasses";
+import { Inter_Tight } from "next/font/google";
 import Image from "next/image";
+
+const interTight = Inter_Tight({ subsets: ["latin"] });
 
 export default function FeaturedImage() {
 	return (
-		<figure className="relative grid place-items-center w-full h-80 rounded-lg overflow-hidden [&>*]:col-span-full [&>*]:row-span-full shadow-md">
-			<div className="w-full h-full dark:bg-[#111210]/60 z-10" />
-			<figcaption className="z-20 px-4">
-				<h1 className="max-w-[24ch] text-4xl dark:text-[#ede0de]">Filling out a form has never been easier.</h1>
-				<sub className="dark:text-[#ede0de] uppercase font-medium">Online forms made easy</sub>
-			</figcaption>
-			<Image
-				src="/images/government_plaza.jpg"
-				alt="Hero image"
-				className="-z-10 object-cover"
-				fill
-			/>
-		</figure>
+		<>
+			<figure className="relative grid place-items-center w-full h-80 rounded-lg overflow-hidden aspect-video *:col-span-full *:row-span-full shadow-md">
+				<div className="w-full h-full dark:opacity-60 bg-[var(--gray-1)] opacity-30 z-10 transition-colors" />
+				<figcaption className="z-20 px-4 dark:text-[#ede0de] text-gray-12">
+					<h1 className={joinClasses( interTight.className,"max-w-[24ch] text-5xl")}>Sign-up, Simplified.</h1>
+					<sub className="uppercase font-medium">Online forms made easy</sub>
+				</figcaption>
+				<Image
+					src="/images/government_plaza.jpg"
+					alt="Hero image"
+					className="-z-10 object-cover"
+					fill
+				/>
+			</figure>
+		</>
 	);
 }
