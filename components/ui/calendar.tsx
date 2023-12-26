a0509d9b8dd1f7b7980d7fb1d7eb8495412addfb
Stephan Wilson <stephanthedev@gmail.com>
2023-12-26 19:26:25 -0400

    updated styles

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -25,7 +25,7 @@ function Calendar({
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         nav_button: joinClasses(
-          // buttonVariants({ variant: "outline" }),
+          "inline-flex justify-center items-center border border-grayA-4 bg-transparent shadow-sm hover:bg-grayA-4 hover:text-gray-11 rounded-md",
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
         ),
         nav_button_previous: "absolute left-1",
@@ -37,13 +37,13 @@ function Calendar({
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: joinClasses(
-          // buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
+          "bg-transparent rounded-md shadow-sm hover:bg-grayA-4 hover:text-gray-12",
+          "h-8 w-8 p-0 font-normal aria-selected:opacity-100"
         ),
         day_range_end: "day-range-end",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
+          "bg-grayA-4 text-gray-12 focus:bg-grayA-4 focus:text-gray-12 hover:bg-grayA-4 hover:bg-text-gray-12",
+        day_today: "bg-accent-9-contrast text-gray-1",
         day_outside:
           "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
         day_disabled: "text-muted-foreground opacity-50",
