65b2e761dad6f04943e25ade02dfc2f5bcf9f1c8
Stephan Wilson <stephanthedev@gmail.com>
2024-01-14 12:52:11 -0400

    data: navLinks

---
+++ data/navLinks.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { title: "Company", href: "/registry/company" },
+  { title: "Civil", href: "/registry/civil" },
+  { title: "Land", href: "/registry/land" },
+  { title: "Digital", href: "/registry/digital" },
+];
