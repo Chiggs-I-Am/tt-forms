9384f59d6e421d747b47c1e617e7791fe06d8c61
Stephan Wilson <stephanthedev@gmail.com>
2024-01-14 13:11:33 -0400

    page: civil

---
+++ app/registry/civil/page.tsx
@@ -0,0 +1,11 @@
+import { Heading, Text } from "@radix-ui/themes";
+
+export default function Page() {
+  return (
+    <main className="[grid-area:main/fullbleed]">
+      <section className="[grid-area:main]">
+        <Heading>Civil registry forms</Heading>
+      </section>
+    </main>
+  );
+}
