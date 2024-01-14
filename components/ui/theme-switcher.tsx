8f9eb33ab75b68c211d938000bc51675b0d797a4
Stephan Wilson <stephanthedev@gmail.com>
2024-01-14 12:40:20 -0400

    updated format

--- components/ui/theme-switcher.tsx
+++ components/ui/theme-switcher.tsx
@@ -4,22 +4,22 @@ import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { IconButton } from "@radix-ui/themes";
 import { useTheme } from "next-themes";
 
-export default function ThemeSwitcher() 
-{
+export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
 
   return (
-		<>
-			<IconButton
+    <>
+      <IconButton
         variant="ghost"
         color="sky"
-				onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        { theme !== "light" ? 
-          ( <MoonIcon className="w-4 h-4" /> )
-          : 
-          ( <SunIcon className="w-4 h-4" /> )
-        }
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      >
+        {theme !== "light" ? (
+          <MoonIcon className="h-4 w-4" />
+        ) : (
+          <SunIcon className="h-4 w-4" />
+        )}
       </IconButton>
-		</>
-	);
+    </>
+  );
 }
