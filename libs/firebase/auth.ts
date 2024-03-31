f37b486bb6e75cd5b26cbb6745d57a2826a8e2ef
Stephan Wilson <stephanthedev@gmail.com>
2024-03-31 13:48:06 -0400

    updated auth

--- libs/firebase/auth.ts
+++ libs/firebase/auth.ts
@@ -1,74 +1,69 @@
 import { ActionCodeSettings, browserSessionPersistence, getAdditionalUserInfo, getIdToken, GoogleAuthProvider, isSignInWithEmailLink, onAuthStateChanged, sendSignInLinkToEmail, setPersistence, signInWithEmailLink, signInWithPopup, UserCredential } from "firebase/auth";
 import { NextRouter } from "next/router";
-import { getFirebase } from "./firebaseApp";
+import { getFirebase } from "./app";
 import { createUserSession } from "./firestore";
 
 const { auth } = getFirebase();
 
-export function onAuth( callback: Function )
-{
-  return onAuthStateChanged( auth, ( user ) => {
-    callback( user );
+export function onAuth(callback: Function) {
+  return onAuthStateChanged(auth, (user) => {
+    callback(user);
   })
 }
 
-export async function signInWithGoogle() 
-{
+export async function signInWithGoogle() {
   const provider = new GoogleAuthProvider();
 
-  await setPersistence( auth, browserSessionPersistence );
+  await setPersistence(auth, browserSessionPersistence);
 
-  const userCredential = await signInWithPopup( auth, provider );
+  const userCredential = await signInWithPopup(auth, provider);
 
-  const idToken = await getIdToken( userCredential.user )
-  createUserSession( idToken, userCredential.user.uid );
+  const idToken = await getIdToken(userCredential.user)
+  createUserSession(idToken, userCredential.user.uid);
 
   return userCredential;
 }
 
-export function additionalUserInfo( userCredential: UserCredential )
-{
-  let userInfo = getAdditionalUserInfo( userCredential );
+export function additionalUserInfo(userCredential: UserCredential) {
+  let userInfo = getAdditionalUserInfo(userCredential);
 
   return userInfo;
 }
 
 
-export async function signInWithEmail( email: string, url: string )
-{
+export async function signInWithEmail(email: string, url: string) {
   const actionCodeSettings: ActionCodeSettings = {
     url,
     handleCodeInApp: true,
   };
-  
-  try { 
-    await sendSignInLinkToEmail( auth, email, actionCodeSettings );
-    window.localStorage.setItem( "emailForSignIn", email );
+
+  try {
+    await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+    window.localStorage.setItem("emailForSignIn", email);
   }
-  catch( error: any ) {
-    console.log( error.code, error.message );
+  catch (error: any) {
+    console.log(error.code, error.message);
   }
 }
 
-export async function checkEmailSignIn( email: string, router: NextRouter )
-{
-  if( isSignInWithEmailLink( auth, window.location.href ) ) {
-    if( !email ) {
+export async function checkEmailSignIn(email: string, router: NextRouter) {
+  if (isSignInWithEmailLink(auth, window.location.href)) {
+    if (!email) {
       // prompt user for email address
-      console.log( "Sign in again!" );
+      console.log("Sign in again!");
     }
     try {
-      let userCredential = await signInWithEmailLink( auth, email!, window.location.href );
+      let userCredential = await signInWithEmailLink(auth, email!, window.location.href);
 
-      let additionalInfo = additionalUserInfo( userCredential );
-      if( additionalInfo?.isNewUser ) {
+      let additionalInfo = additionalUserInfo(userCredential);
+      if (additionalInfo?.isNewUser) {
         router.push("/auth/create-username");
       }
-      
-      window.localStorage.removeItem( "emailForSignIn" );
+
+      window.localStorage.removeItem("emailForSignIn");
     }
-    catch( error: any ) {
-      console.log( error.code, error.message );
+    catch (error: any) {
+      console.log(error.code, error.message);
     }
   }
 }
